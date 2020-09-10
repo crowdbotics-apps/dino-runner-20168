aade69c065d5ce34b82c9367d57bce41582eeec4
Crowdbotics <team@crowdbotics.com>
2020-09-10 01:14:05 +0000

    Initial commit.

---
+++ ios/dino_runner_20168Tests/dino_runner_20168Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface dino_runner_20168 : XCTestCase
+
+@end
+
+@implementation dino_runner_20168
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
